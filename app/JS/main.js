77b4f22eadde35ab330019dc5f8d684ab94ea4e4
hambergss <144480774+hambergss@users.noreply.github.com>
2024-12-16 14:00:31 -0500

    v

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,51 @@
 import "../css/style.css";
+
+const words = {
+  easy: [
+    "apple",
+    "ball",
+    "cat",
+    "dog",
+    "house",
+    "tree",
+    "book",
+    "car",
+    "sun",
+    "happy",
+  ],
+  medium: [
+    "library",
+    "journey",
+    "mystery",
+    "puzzle",
+    "tiger",
+    "guitar",
+    "storm",
+    "planet",
+    "river",
+    "chicken",
+  ],
+  hard: [
+    "metamorphosis",
+    "subterranean",
+    "conglomerate",
+    "ephemeral",
+    "quixotic",
+    "paradox",
+    "cacophony",
+    "serendipity",
+    "susceptible",
+    "magnanimous",
+  ],
+};
+
+const DOMSelectors = {
+  container: document.getElementById("main-container"),
+  easyModeBtn: document.getElementById("easyMode-btn"),
+  mediumModeBtn: document.getElementById("mediumMode-btn"),
+  hardModeBtn: document.getElementById("hardMode-btn"),
+  easySubBtn: document.getElementById("easySub-btn"),
+  mediumSubBtn: document.getElementById("mediumSub-btn"),
+  hardSubBtn: document.getElementById("hardSub-btn"),
+  
+};
